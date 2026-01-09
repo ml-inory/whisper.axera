34dc09326960f9570ae3816a3719689669c6d39f
yangrongzhao <yangrongzhao@axera-tech.com>
2026-01-09 14:49:39 +0800

    Change default param for cpp

--- cpp/whisper_srv.cpp
+++ cpp/whisper_srv.cpp
@@ -12,7 +12,7 @@ int main(int argc, char** argv) {
     cmdline::parser cmd;
     cmd.add<int>("port", 'p', "http port", false, 8080);
     cmd.add<std::string>("model_type", 't', "tiny, base, small, turbo, large", false, "turbo");
-    cmd.add<std::string>("model_path", 0, "model path for *.axmodel, tokens.txt, positional_embedding.bin", false, "../models/models-ax650");
+    cmd.add<std::string>("model_path", 0, "model path for *.axmodel, tokens.txt, positional_embedding.bin", false, "../models-ax650");
     cmd.add<std::string>("language", 'l', "en, zh", false, "zh");
     cmd.parse_check(argc, argv);
 
--- models/models-ax650/turbo/download.sh
+++
@@ -1,6 +0,0 @@
-wget https://hf-mirror.com/AXERA-TECH/Whisper/resolve/main/models-ax650/turbo/turbo-decoder-loop.axmodel
-wget https://hf-mirror.com/AXERA-TECH/Whisper/resolve/main/models-ax650/turbo/turbo-decoder-main.axmodel
-wget https://hf-mirror.com/AXERA-TECH/Whisper/resolve/main/models-ax650/turbo/turbo-encoder.axmodel
-wget https://hf-mirror.com/AXERA-TECH/Whisper/resolve/main/models-ax650/turbo/turbo-positional_embedding.bin
-wget https://hf-mirror.com/AXERA-TECH/Whisper/resolve/main/models-ax650/turbo/turbo-tokens.txt
-wget https://hf-mirror.com/AXERA-TECH/Whisper/resolve/main/models-ax650/turbo/turbo_config.json
